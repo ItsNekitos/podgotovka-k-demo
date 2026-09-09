ee8b43040e8512d39e0806518b4f9e52460f97b2
ItsNekitos <nik-gusev-07@mail.ru>
2026-09-09 14:37:27 +0500

    bases

---
+++ app/Models/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['place_name', 'date', 'oplata'])]
+class Order extends Model
+{
+    //
+}
