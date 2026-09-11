f041fd6ee6743747e8a9c4d3cd7db20c7a448aca
ItsNekitos <nik-gusev-07@mail.ru>
2026-09-11 15:47:56 +0500

    ксс

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,29 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 
 <body>
-    <x-header/>
+    <x-header />
     <main>
-            <form action="{{ Route('login') }}" method="get">
-                @csrf
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Ваше имя</label>
-                    <input type="text" value="{{ old('name') }}" name="name" class="form-control" id="exampleInputPassword1" placeholder="ФИО">
-                    @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+        <div id="maindiv">
+            <div class="sidediv"></div>
+            <div id="osnova">
+                <div id="zagolovok">
+                    <h1>Вход</h1>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Пароль</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Введите пароль">
-                    @error('password')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <button type="submit" class="btn btn-primary" style="background-color:rgb(244, 126, 0); border-color: rgb(170, 88, 0);">Войти</button>
-                <a style="margin-left: 25px;" href="{{ Route('viewregister') }}">Регистрация</a>
-            </form>
+                <form action="{{ Route('login') }}" method="get">
+                    @csrf
+                    <div>
+                        <label for="exampleInputPassword1" class="form-label">Ваше имя</label>
+                        <input type="text" value="{{ old('name') }}" name="name" class="form-control" id="exampleInputPassword1" placeholder="Имя">
+                        @error('name')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="exampleInputPassword1" class="form-label">Пароль</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Введите пароль">
+                        @error('password')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary" style="background-color:rgb(244, 126, 0); border-color: rgb(170, 88, 0);">Войти</button>
+                        <a href="{{ Route('viewregister') }}">Нет аккаунта? Зарегестрируйтесь</a>
+                    </div>
+                </form>
+            </div>
+            <div class="sidediv"></div>
+        </div>
     </main>
 </body>
 
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,36 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 
 <body>
-    <x-header/>
+    <x-header />
     <main>
-            <form action="{{ Route('register') }}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Ваше имя</label>
-                    <input type="text" value="{{ old('name') }}" name="name" class="form-control" id="exampleInputPassword1" placeholder="ФИО">
-                    @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+        <div id="maindiv">
+            <div class="sidediv"></div>
+            <div id="osnova">
+                <div id="zagolovok">
+                    <h1>Регистрация</h1>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">E-mail</label>
-                    <input type="email" value="{{ old('email') }}" name="email" class="form-control" id="exampleInputPassword1" placeholder="Введите E-mail">
-                    @error('email')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Пароль</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Введите пароль">
-                    @error('password')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <button type="submit" class="btn btn-primary" style="background-color:rgb(244, 126, 0); border-color: rgb(170, 88, 0);">Зарегистрироваться</button>
-                <a style="margin-left: 25px;" href="{{ Route('viewlogin') }}">Вход в аккаунт</a>
-            </form>
+                <form action="{{ Route('register') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Ваше имя</label>
+                        <input type="text" value="{{ old('name') }}" name="name" class="form-control" id="exampleInputPassword1" placeholder="ФИО">
+                        @error('name')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">E-mail</label>
+                        <input type="email" value="{{ old('email') }}" name="email" class="form-control" id="exampleInputPassword1" placeholder="Введите E-mail">
+                        @error('email')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Пароль</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Введите пароль">
+                        @error('password')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary" style="background-color:rgb(244, 126, 0); border-color: rgb(170, 88, 0);">Зарегистрироваться</button>
+                        <a href="{{ Route('viewlogin') }}">Вход в аккаунт</a>
+                    </div>
+                </form>
+            </div>
+            <div class="sidediv"></div>
+        </div>
     </main>
 </body>
 
